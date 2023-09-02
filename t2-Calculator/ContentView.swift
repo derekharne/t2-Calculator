0d43ef576188df3e98dac2b70c1ee68af1c3f541
Derek Harne <44200680+derekharne@users.noreply.github.com>
2023-09-02 15:09:22 -0500

    Initial Commit

---
+++ t2-Calculator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  t2-Calculator
+//
+//  Created by Derek Harne on 9/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ t2-Calculator/t2_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  t2_CalculatorApp.swift
+//  t2-Calculator
+//
+//  Created by Derek Harne on 9/2/23.
+//
+
+import SwiftUI
+
+@main
+struct t2_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
